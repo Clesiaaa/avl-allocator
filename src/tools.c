7c88f2f2b6b324182c32c82f1bfced53425773a9
Clesiaaa <robert.folga@outlook.com>
2026-02-26 23:09:34 +0100

    insert_val

--- src/tools.c
+++ src/tools.c
@@ -11,11 +11,46 @@ int8_t balance(ball_t *ball) {
     return height(ball->left) - height(ball->right);
 }
 
-/*
+
 ball_t *insert_avl(ball_t *ball, ball_t *a) {
-    printf("Not Implemented!");
+    
+    if (ball == NULL) return a;
+    else if (a->size < ball->size) { 
+        ball->left = insert_avl(ball->left, a);
+        return equilibrate(ball);
+    }
+    else if (a->size == ball->size) { 
+        ball->left = insert_avl(ball->left, a);
+        return equilibrate(ball);
+    }
+    else if (a->size > ball->size) { 
+        ball->right = insert_avl(ball->right, a);
+        return equilibrate(ball);    
+    }
 };
-*/
+
+
+ball_t *equilibrate(ball_t *ball){
+    
+    if (balance(ball) < 1) {
+        if (balance(ball->left) >= 0) {
+            return rotate_right(ball);
+        } else {
+            ball->left = rotate_right(ball->left);
+            return rotate_left(ball);
+        }
+    }
+
+    if (balance(ball) > 1) {
+        if (balance(ball) >= 0) {
+            return rotate_left(ball);
+        } else {
+            ball->right = rotate_right(ball->right);
+            return rotate_left(ball);
+        }
+    }
+    return ball;
+}
 
 ball_t *rotate_left(ball_t *ball) {
     ball_t *temp = ball;
@@ -35,8 +70,7 @@ ball_t *rotate_right(ball_t *ball) {
     return ball;
 };
 
-/*
+
 ball_t *delete_val(ball_t *ball, size_t size) {
-    printf("Not Implemented!");
+    printf("Not Implemented!\n");
 };
-*/
