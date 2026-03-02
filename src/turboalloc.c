5b07f34b298554d590c19e51b57613b113a3e59f
Clesiaaa <robert.folga@outlook.com>
2026-03-02 21:19:07 +0100

    fixes t_callocw

--- src/turboalloc.c
+++ src/turboalloc.c
@@ -6,6 +6,7 @@
 
 
 ball_t *heap_startw = NULL;
+ball_t *start = NULL;
 void *t_allocw(size_t size) {
     
     ball_t *current_ball = heap_startw;
@@ -37,7 +38,7 @@ void t_freew(void *ptr) {
 void *t_callocw(size_t nb, size_t size)
 {       
     void *first_ptr = NULL;
-    ball_t *current_ball = heap_startw;
+    ball_t *current_ball = start;
 
     for (uint16_t i = 0x00; i < nb; i++) {
 
@@ -45,13 +46,13 @@ void *t_callocw(size_t nb, size_t size)
             ball_t *new_ball_root = sbrk(size + sizeof(ball_t));
             if (new_ball_root == (void *)-1) return NULL;
             *new_ball_root = create_ball(size);
-            heap_startw = new_ball_root;
-            if (first_ptr == NULL) first_ptr = (char *)heap_startw + sizeof(ball_t);
+            start = new_ball_root;
+            if (first_ptr == NULL) first_ptr = (char *)start + sizeof(ball_t);
         } else {
             ball_t *new_ball = sbrk(size + sizeof(ball_t));
             if (new_ball == (void *)-1) return NULL; 
             *new_ball = create_ball(size);
-            heap_startw = insert_avl(current_ball, new_ball);
+            start = insert_avl(current_ball, new_ball);
             if (first_ptr == NULL) first_ptr = (char *)new_ball + sizeof(ball_t);
         }
     }
